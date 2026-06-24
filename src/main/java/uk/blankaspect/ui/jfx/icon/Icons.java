d09cafe1728abd6adf06d4109c70f5dd0ce89857
amr <id883436@prng.uk>
2026-06-24 06:12:38 +0100

    Added the 'clear02' methods.
    Renamed constants and parameters of the 'clear01' methods.

--- src/main/java/uk/blankaspect/ui/jfx/icon/Icons.java
+++ src/main/java/uk/blankaspect/ui/jfx/icon/Icons.java
@@ -26,6 +26,7 @@ import javafx.scene.shape.Circle;
 import javafx.scene.shape.LineTo;
 import javafx.scene.shape.MoveTo;
 import javafx.scene.shape.Path;
+import javafx.scene.shape.Polygon;
 import javafx.scene.shape.Rectangle;
 import javafx.scene.shape.StrokeLineCap;
 
@@ -50,23 +51,50 @@ public class Icons
 //  Constants
 ////////////////////////////////////////////////////////////////////////
 
-	/** The factor by which the default text height is multiplied to give the width and height of the <i>clear01</i>
+	/** The factor by which the height of the default font is multiplied to give the default size of the <i>clear01</i>
 		icon. */
 	public static final		double	CLEAR01_DEFAULT_SIZE_FACTOR	= 0.8;
 
-	/** The factor by which the radius of the disc of the <i>clear01</i> icon is multiplied to give a coordinate of
-		the cross of the icon. */
-	private static final	double	CLEAR01_CROSS_SIZE_FACTOR	= 0.4;
+	/** The factor by which the size of the <i>clear01</i> icon is multiplied to give the radius of the background
+		disc. */
+	private static final	double	CLEAR01_RADIUS_FACTOR	= 0.5;
 
-	/** The factor by which the radius of the disc of the <i>clear01</i> icon is multiplied to give the width of the
-		strokes of the cross. */
-	private static final	double	CLEAR01_CROSS_STROKE_WIDTH_FACTOR	= 0.3;
+	/** The factor by which the size of the <i>clear01</i> icon is multiplied to give a coordinate of the cross of the
+		icon. */
+	private static final	double	CLEAR01_CROSS_SIZE_FACTOR	= 0.4 * CLEAR01_RADIUS_FACTOR;
+
+	/** The factor by which the size of the <i>clear01</i> icon is multiplied to give the width of the strokes of the
+		cross. */
+	private static final	double	CLEAR01_CROSS_STROKE_WIDTH_FACTOR	= 0.3 * CLEAR01_RADIUS_FACTOR;
+
+	/** The factor by which the height of the default font is multiplied to give the default height of the
+		<i>clear02</i> icon. */
+	public static final		double	CLEAR02_DEFAULT_HEIGHT_FACTOR	= 0.8;
+
+	/** The factor by which the height of the <i>clear02</i> icon is multiplied to give the <i>a</i> dimension. */
+	private static final	double	CLEAR02_A_FACTOR	= 0.5;
+
+	/** The factor by which the height of the <i>clear02</i> icon is multiplied to give the <i>b</i> dimension. */
+	private static final	double	CLEAR02_B_FACTOR	= 0.85;
+
+	/** The factor by which the height of the <i>clear02</i> icon is multiplied to give the <i>d</i> dimension. */
+	private static final	double	CLEAR02_D_FACTOR	= 0.7 * CLEAR02_A_FACTOR;
+
+	/** The factor by which the height of the <i>clear02</i> icon is multiplied to give a coordinate of the cross of the
+		icon. */
+	private static final	double	CLEAR02_CROSS_SIZE_FACTOR	= 0.4 * CLEAR02_A_FACTOR;
+
+	/** The factor by which the height of the <i>clear02</i> icon is multiplied to give the width of the strokes of the
+		cross. */
+	private static final	double	CLEAR02_CROSS_STROKE_WIDTH_FACTOR	= 0.3 * CLEAR02_A_FACTOR;
 
 	/** CSS style classes. */
 	public interface StyleClass
 	{
-		String	CLEAR01_CROSS	= StyleConstants.CLASS_PREFIX + "icons-clear01-cross";
-		String	CLEAR01_DISC	= StyleConstants.CLASS_PREFIX + "icons-clear01-disc";
+		String	CLEAR01_BACKGROUND	= StyleConstants.CLASS_PREFIX + "icons-clear01-background";
+		String	CLEAR01_FOREGROUND	= StyleConstants.CLASS_PREFIX + "icons-clear01-foreground";
+		String	CLEAR02_BACKGROUND	= StyleConstants.CLASS_PREFIX + "icons-clear02-background";
+		String	CLEAR02_FOREGROUND	= StyleConstants.CLASS_PREFIX + "icons-clear02-foreground";
 	}
 
 ////////////////////////////////////////////////////////////////////////
@@ -88,27 +116,27 @@ public class Icons
 ////////////////////////////////////////////////////////////////////////
 
 	public static Group clear01(
-		Color	discColour,
-		Color	crossColour)
+		Color	backgroundColour,
+		Color	foregroundColour)
 	{
-		return clear01(discColour, crossColour, CLEAR01_DEFAULT_SIZE_FACTOR, CLEAR01_DEFAULT_SIZE_FACTOR);
+		return clear01(backgroundColour, foregroundColour, CLEAR01_DEFAULT_SIZE_FACTOR, CLEAR01_DEFAULT_SIZE_FACTOR);
 	}
 
 	//------------------------------------------------------------------
 
 	public static Group clear01(
-		Color	discColour,
-		Color	crossColour,
+		Color	backgroundColour,
+		Color	foregroundColour,
 		double	sizeFactor)
 	{
-		return clear01(discColour, crossColour, sizeFactor, sizeFactor);
+		return clear01(backgroundColour, foregroundColour, sizeFactor, sizeFactor);
 	}
 
 	//------------------------------------------------------------------
 
 	public static Group clear01(
-		Color	discColour,
-		Color	crossColour,
+		Color	backgroundColour,
+		Color	foregroundColour,
 		double	sizeFactor,
 		double	tileSizeFactor)
 	{
@@ -119,15 +147,14 @@ public class Icons
 		// Calculate size of icon from height of default font
 		double textHeight = TextUtils.textHeight();
 		double size = sizeFactor * textHeight;
-		double tileSize = Math.ceil(tileSizeFactor * textHeight);
 
 		// Create disc
-		double radius = 0.5 * size;
-		Circle disc = new Circle(radius, discColour);
-		disc.getStyleClass().add(StyleClass.CLEAR01_DISC);
+		double radius = CLEAR01_RADIUS_FACTOR * size;
+		Circle disc = new Circle(radius, backgroundColour);
+		disc.getStyleClass().add(StyleClass.CLEAR01_BACKGROUND);
 
 		// Create cross
-		double coord = CLEAR01_CROSS_SIZE_FACTOR * radius;
+		double coord = CLEAR01_CROSS_SIZE_FACTOR * size;
 		Path cross = new Path
 		(
 			new MoveTo(-coord, -coord),
@@ -135,18 +162,96 @@ public class Icons
 			new MoveTo(-coord, coord),
 			new LineTo(coord, -coord)
 		);
-		cross.setStroke(crossColour);
-		cross.setStrokeWidth(radius * CLEAR01_CROSS_STROKE_WIDTH_FACTOR);
+		cross.setStroke(foregroundColour);
+		cross.setStrokeWidth(CLEAR01_CROSS_STROKE_WIDTH_FACTOR * size);
 		cross.setStrokeLineCap(StrokeLineCap.ROUND);
-		cross.getStyleClass().add(StyleClass.CLEAR01_CROSS);
+		cross.getStyleClass().add(StyleClass.CLEAR01_FOREGROUND);
 
 		// Create background rectangle
+		double tileSize = Math.ceil(tileSizeFactor * textHeight);
 		double tileOffset = -0.5 * tileSize;
 		Rectangle background = new Rectangle(tileOffset, tileOffset, tileSize, tileSize);
 		background.setFill(Color.TRANSPARENT);
 
 		// Create and return icon
 		return new Group(background, disc, cross);
+	}
+
+	//------------------------------------------------------------------
+
+	public static Group clear02(
+		Color	backgroundColour,
+		Color	foregroundColour)
+	{
+		return clear02(backgroundColour, foregroundColour, CLEAR02_DEFAULT_HEIGHT_FACTOR,
+					   CLEAR02_DEFAULT_HEIGHT_FACTOR);
+	}
+
+	//------------------------------------------------------------------
+
+	public static Group clear02(
+		Color	backgroundColour,
+		Color	foregroundColour,
+		double	sizeFactor)
+	{
+		return clear02(backgroundColour, foregroundColour, sizeFactor, sizeFactor);
+	}
+
+	//------------------------------------------------------------------
+
+	public static Group clear02(
+		Color	backgroundColour,
+		Color	foregroundColour,
+		double	heightFactor,
+		double	tileHeightFactor)
+	{
+		// Validate arguments
+		if (heightFactor > tileHeightFactor)
+			throw new IllegalArgumentException("Height factor is greater than tile-height factor");
+
+		// Calculate height of icon from height of default font
+		double textHeight = TextUtils.textHeight();
+		double height = heightFactor * textHeight;
+
+		// Create polygon
+		double a = CLEAR02_A_FACTOR * height;
+		double b = CLEAR02_B_FACTOR * height;
+		double c = b - a;
+		double d = CLEAR02_D_FACTOR * height;
+		double x1 = -(c + d);
+		double x2 = -c;
+		double x3 = a;
+		double y1 = -a;
+		double y2 = 0.0;
+		double y3 = a;
+		Polygon polygon = new Polygon(x1, y2, x2, y1, x3, y1, x3, y3, x2, y3);
+		polygon.getStyleClass().add(StyleClass.CLEAR02_BACKGROUND);
+
+		// Create cross
+		double coord = CLEAR02_CROSS_SIZE_FACTOR * height;
+		Path cross = new Path
+		(
+			new MoveTo(-coord, -coord),
+			new LineTo(coord, coord),
+			new MoveTo(-coord, coord),
+			new LineTo(coord, -coord)
+		);
+		cross.setStroke(foregroundColour);
+		cross.setStrokeWidth(CLEAR02_CROSS_STROKE_WIDTH_FACTOR * height);
+		cross.setStrokeLineCap(StrokeLineCap.ROUND);
+		cross.getStyleClass().add(StyleClass.CLEAR02_FOREGROUND);
+
+		// Create background rectangle
+		double h = tileHeightFactor * textHeight;
+		double tileHeight = Math.ceil(h);
+		double tileWidth = Math.ceil(h / (2.0 * a) * (b + d));
+		double tileYOffset = -0.5 * tileHeight;
+		double tileXOffset = -tileWidth - tileYOffset;
+		Rectangle background = new Rectangle(tileXOffset, tileYOffset, tileWidth, tileHeight);
+		background.setFill(Color.TRANSPARENT);
+
+		// Create and return icon
+		return new Group(background, polygon, cross);
 	}
 
 	//------------------------------------------------------------------
